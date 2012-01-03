3460fc79988993467fcd5b4bbe55836c669a4b9e
Andy Shin <andy@togetherville.com>
2012-01-03 10:52:09 -0800

    hw2

---
+++ hw2/Calculator/Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Andrew Shin on 12/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CalculatorViewController;
+
+@interface CalculatorAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet CalculatorViewController *viewController;
+
+@end
